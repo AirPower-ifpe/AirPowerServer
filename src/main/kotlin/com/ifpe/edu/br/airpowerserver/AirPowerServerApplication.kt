d197a428ad783b5ce3dbfbcdae28e8f7575a497a
Willian Santos <willjsan@motorola.com>
2025-06-01 14:21:17 -0300

    initial commit

---
+++ src/main/kotlin/com/ifpe/edu/br/airpowerserver/AirPowerServerApplication.kt
@@ -0,0 +1,11 @@
+package com.ifpe.edu.br.airpowerserver
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AirPowerServerApplication
+
+fun main(args: Array<String>) {
+    runApplication<AirPowerServerApplication>(*args)
+}
---
+++ src/test/kotlin/com/ifpe/edu/br/airpowerserver/AirPowerServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ifpe.edu.br.airpowerserver
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AirPowerServerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}

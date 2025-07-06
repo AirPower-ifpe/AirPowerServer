84db8cc9fff8036bca65f7d8939fef2b16d3b08c
Willian Santos <willjsan@motorola.com>
2025-07-06 19:15:19 -0300

    Refactor: Adds new param to Alarm data objetct

--- src/main/kotlin/com/ifpe/edu/br/airpowerserver/dto/alarms/APAlarmInfo.kt
+++ src/main/kotlin/com/ifpe/edu/br/airpowerserver/dto/alarms/APAlarmInfo.kt
@@ -1,16 +1,39 @@
 package com.ifpe.edu.br.airpowerserver.dto.alarms
 
-import java.util.UUID
+import com.ifpe.edu.br.airpowerserver.dto.Id
 
 data class APAlarmInfo(
-    val id: UUID,
+    val id: Id,
+    val createdTime: Long,
+    val tenantId: Id,
+    val customerId: Id,
     val type: String,
-    val message: String,
-    val timestamp: Long,
-    val occurrence: Int
-)
-{
+    val originator: Id,
+    val severity: String,
+    val acknowledged: Boolean,
+    val cleared: Boolean,
+    val assigneeId: Id?,
+    val originatorName: String?,
+    val originatorLabel: String?,
+    val assignee: TBAssignee?,
+    val name: String,
+    val status: String
+) {
     override fun toString(): String {
-        return "APAlarmInfo(id=$id, type='$type', message='$message', timestamp=$timestamp, occurrence=$occurrence)"
+        return "APAlarmInfo(id=$id, " +
+                "createdTime=$createdTime, " +
+                "tenantId=$tenantId, " +
+                "customerId=$customerId, " +
+                "type='$type'," +
+                " originator=$originator, " +
+                "severity='$severity'," +
+                " acknowledged=$acknowledged, " +
+                "cleared=$cleared, " +
+                "assigneeId=$assigneeId, " +
+                "originatorName=$originatorName," +
+                " originatorLabel=$originatorLabel," +
+                " assignee=$assignee, " +
+                "name='$name', " +
+                "status='$status')"
     }
 }
